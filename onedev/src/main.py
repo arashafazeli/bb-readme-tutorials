0ed0e29399d5f65a2ae62ad6922884171b960b6f
kjebo <kjellbovin@hotmail.com>
2022-06-01 14:53:52 +0200

    onedev/

---
+++ onedev/src/main.py
@@ -0,0 +1,15 @@
+"""This is just an example script to try out OneDev. And its all good!
+"""
+
+
+import pandas as pd
+
+
+def test():
+    """This is just a test function
+    """
+    print(pd.DataFrame())
+
+
+if __name__ == "__main__":
+    test()
